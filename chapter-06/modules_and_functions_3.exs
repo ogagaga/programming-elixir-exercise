134dee3a382e2f80171b541649b6f5f7d84accdf
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-09-27 13:02:05 +0900

    Add Times exercises

---
+++ chapter-06/modules_and_functions_3.exs
@@ -0,0 +1,16 @@
+defmodule Times do
+  # def double(n) do
+  #   n * 2
+  # end
+  def double(n), do: n * 2
+
+  # def triple(n) do
+  #   n * 3
+  # end
+  def triple(n), do: n * 3
+
+  # def quadruple(n) do
+  #   double(n) * 2
+  # end
+  def quadruple(n), do: double(n) * 2
+end
