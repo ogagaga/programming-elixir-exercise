557477013b727f68ce477af563d29f789a301b52
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-09-28 13:35:27 +0900

    Add ModulesAndFunctions-6 exercises

--- chapter-06/modules_and_functions_6.exs
+++ chapter-06/modules_and_functions_6.exs
@@ -1,28 +1,28 @@
 defmodule Chop do
   # https://github.com/laurocaetano/programming-elixir-exercises/blob/master/ch-06/chop.exs
-  def guess(actual, low..high) do
-    first_guess = div(low + high, 2)
-    IO.puts "Is it #{first_guess}?"
-    make_guess(actual, low..high, first_guess)
-  end
+  # def guess(actual, low..high) do
+  #   first_guess = div(low + high, 2)
+  #   IO.puts "Is it #{first_guess}?"
+  #   make_guess(actual, low..high, first_guess)
+  # end
 
-  def make_guess(actual, _, current_guess) when actual == current_guess do
-    current_guess
-    IO.puts "Is it #{current_guess}?"
-    IO.puts "Yes, it's #{current_guess}."
-  end
+  # def make_guess(actual, _, current_guess) when actual == current_guess do
+  #   current_guess
+  #   IO.puts "Is it #{current_guess}?"
+  #   IO.puts "Yes, it's #{current_guess}."
+  # end
 
-  def make_guess(actual, low.._, current_guess) when current_guess > actual do
-    next_guess = div(low + (current_guess - 1), 2)
-    IO.puts "Is it #{next_guess}?"
-    make_guess(actual, low..current_guess, next_guess)
-  end
+  # def make_guess(actual, low.._, current_guess) when current_guess > actual do
+  #   next_guess = div(low + (current_guess - 1), 2)
+  #   IO.puts "Is it #{next_guess}?"
+  #   make_guess(actual, low..current_guess, next_guess)
+  # end
 
-  def make_guess(actual, _..high, current_guess) when current_guess < actual do
-    next_guess = div((current_guess + 1) + high, 2)
-    IO.puts "Is it #{next_guess}?"
-    make_guess(actual, current_guess..high, next_guess)
-  end
+  # def make_guess(actual, _..high, current_guess) when current_guess < actual do
+  #   next_guess = div((current_guess + 1) + high, 2)
+  #   IO.puts "Is it #{next_guess}?"
+  #   make_guess(actual, current_guess..high, next_guess)
+  # end
 
   # ガード節ありバージョン
   # def guess(actual, low..high) when actual == div(low + high, 2) do
@@ -43,20 +43,20 @@ defmodule Chop do
   # end
 
   # ガード節なしバージョン
-  # def guess(actual, low..high) do
-  #   midpoint = low + div(high - low, 2)
-  #   IO.puts "-------------------------------------------------------"
-  #   IO.puts "Midpoint is #{midpoint} [low : #{low} / high : #{high}]"
+  def guess(actual, low..high) do
+    midpoint = low + div(high - low, 2)
+    IO.puts "-------------------------------------------------------"
+    IO.puts "Midpoint is #{midpoint} [low : #{low} / high : #{high}]"
 
-  #   cond do
-  #     actual < midpoint ->
-  #       IO.puts "Is it #{midpoint}? => lower"
-  #       guess(actual, low..midpoint)
-  #     actual > midpoint ->
-  #       IO.puts "Is it #{midpoint}? => higher"
-  #       guess(actual, midpoint..high)
-  #     actual == midpoint ->
-  #       IO.puts "Yes, it's #{actual}."
-  #   end
-  # end
+    cond do
+      actual < midpoint ->
+        IO.puts "Is it #{midpoint}? => lower"
+        guess(actual, low..midpoint)
+      actual > midpoint ->
+        IO.puts "Is it #{midpoint}? => higher"
+        guess(actual, midpoint..high)
+      actual == midpoint ->
+        IO.puts "Yes, it's #{actual}."
+    end
+  end
 end
