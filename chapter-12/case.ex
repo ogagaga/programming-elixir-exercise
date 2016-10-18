31bad7470ee79381cbdf6286de2e0b4f7be6db99
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-10-18 16:39:59 +0900

    Add `case` exercise

---
+++ chapter-12/case.ex
@@ -0,0 +1,9 @@
+case File.open("case.ex") do
+  { :ok, file } ->
+    IO.puts "First line: #{IO.read(file, :line)}"
+  { :error, reason } ->
+    IO.puts "Failed to open file: #{reason}"
+end
+
+# First line: case File.open("case.ex") do
+# 
