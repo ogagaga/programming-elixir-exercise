203cba7f3771ed5758d6807197210a5f4c1fc18a
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-10-24 18:09:26 +0900

    Use escript

--- chapter-13/issues/lib/issues/cli.ex
+++ chapter-13/issues/lib/issues/cli.ex
@@ -9,7 +9,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     # process(parse_args(argv))
     argv
     |> parse_args
