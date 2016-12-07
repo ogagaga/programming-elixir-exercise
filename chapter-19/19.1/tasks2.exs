49e2d6b2c136015a1203320e2b36abc405cf67b6
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-12-07 15:12:20 +0900

    Add tasks exercises

---
+++ chapter-19/19.1/tasks2.exs
@@ -0,0 +1,20 @@
+defmodule Fib do
+  def of(0), do: 0
+  def of(1), do: 1
+  def of(n), do: Fib.of(n-1) + Fib.of(n-2)
+
+# fib(5)
+# = fib(4)
+# = fib(3)+ fib(3)
+# = fib(2)+ fib(2) + fib(2) + fib(1)
+# = fib(1)+ fib(1) + fib(1) + fib(0) + fib(1) + fib(0) + fib(1) + fib(0) + fib(1) + fib(1) + fib(0) + fib(1) + fib(0) + fib(1)
+end
+
+IO.puts "Start the task"
+worker = Task.async(Fib, :of, [20])
+IO.puts "Do something else"
+
+IO.puts "Wait for the task"
+result = Task.await(worker)
+
+IO.puts "The result is  #{result}"
